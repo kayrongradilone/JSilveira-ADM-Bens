862c7100fbc3de380b28f1a508eb79e81bdd0da0
kayrongradilone <kayrongradilone@hotmail.com>
2022-11-09 16:08:33 -0300

    feat: Tema escuro e toggle tema

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
+  const {toggleTheme} = useAppThemeContext()
   return (
     <Routes>
       <Route
         path='/pagina-inicial'
-        element={<Button variant='contained' color='secondary'>Teste</Button>}
+        element={<Button variant='contained' color='primary' onClick={toggleTheme}>Teste</Button>}
       />
       <Route path='*' element={<Navigate to='/pagina-inicial' />} />
     </Routes>
---
+++ src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,54 @@
+import {
+  createContext,
+  useState,
+  useCallback,
+  useMemo,
+  useContext,
+} from "react";
+import { ThemeProvider, Box } from "@mui/material";
+
+import { DarkTheme, LightTheme } from "./../themes";
+
+interface IThemeContextData {
+  themeName: "light" | "dark";
+  toggleTheme: () => void;
+}
+const ThemeContext = createContext({} as IThemeContextData);
+
+export const useAppThemeContext = () => {
+  return useContext(ThemeContext);
+};
+
+interface IAppThemeProviderProps {
+  children: React.ReactNode;
+}
+
+export const AppThemeProvider: React.FC<IAppThemeProviderProps> = ({
+  children,
+}) => {
+  const [themeName, setThemeName] = useState<"light" | "dark">("light");
+
+  const toggleTheme = useCallback(() => {
+    setThemeName((oldThemeName) =>
+      oldThemeName === "light" ? "dark" : "light"
+    );
+  }, []);
+
+  const theme = useMemo(() => {
+    if (themeName === "light") return LightTheme;
+    return DarkTheme;
+  }, [themeName]);
+  return (
+    <ThemeContext.Provider value={{ themeName, toggleTheme }}>
+      <ThemeProvider theme={theme}>
+        <Box
+          width="100vw"
+          height="100VH"
+          bgcolor={theme.palette.background.default}
+        >
+          {children}
+        </Box>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
