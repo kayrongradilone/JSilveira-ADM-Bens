74004c41d96bc9bd30a0b5bcd4fa1f04a5424bff
kayrongradilone <kayrongradilone@hotmail.com>
2022-11-10 18:48:54 -0300

    fix: spacing avatar

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -16,7 +16,7 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
 
           <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
             <Avatar
-              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              sx={{ height: theme.spacing(18), width: theme.spacing(18) }}
               src="https://scontent.ffln1-1.fna.fbcdn.net/v/t1.6435-9/46501964_10215247847922755_2301475907941957632_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=84a396&_nc_eui2=AeE5JjNT9M_P_KMFPPh2I3xXvH6nKE2NOjW8fqcoTY06NdSnqm37GnWKkWKdjkDyWB8&_nc_ohc=7Ho2J5xlx2UAX_Zsa2C&_nc_ht=scontent.ffln1-1.fna&oh=00_AfDw9QOw6lktKW75FPZNGO9thIxBeae5jav9_CWUiWlI_Q&oe=6394C8AB"
             />
           </Box>
