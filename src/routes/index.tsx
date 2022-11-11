faad95bc349bc546658f8cd3cc42a3a8033ec10c
kayrongradilone <kayrongradilone@hotmail.com>
2022-11-11 14:26:17 -0300

    feat: responsividade menu lateral

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,24 @@
-import { Button } from '@mui/material';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { useAppThemeContext } from '../shared/contexts';
+import { Button } from "@mui/material";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useDrawerContext } from "../shared/contexts";
 
 export const AppRoutes = () => {
-  const {toggleTheme} = useAppThemeContext()
+  const { toggleDrawerOpen } = useDrawerContext();
   return (
     <Routes>
       <Route
-        path='/pagina-inicial'
-        element={<Button variant='contained' color='primary' onClick={toggleTheme}>Teste</Button>}
+        path="/pagina-inicial"
+        element={
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={toggleDrawerOpen}
+          >
+            Toggle Drawer Open
+          </Button>
+        }
       />
-      <Route path='*' element={<Navigate to='/pagina-inicial' />} />
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
 };
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,20 +1,46 @@
-import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
-import { Box } from '@mui/system';
-import { ReactNode } from 'react';
-
+import {
+  Avatar,
+  Divider,
+  Drawer,
+  Icon,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import { useDrawerContext } from "../../contexts";
 
 interface IMenuLateralProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
   const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down("sm"));
+  const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
 
   return (
     <>
-      <Drawer variant='permanent'>
-        <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
-
-          <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+      <Drawer
+        open={isDrawerOpen}
+        variant={smDown ? "temporary" : "permanent"}
+        onClose={toggleDrawerOpen}
+      >
+        <Box
+          width={theme.spacing(28)}
+          height="100%"
+          display="flex"
+          flexDirection="column"
+        >
+          <Box
+            width="100%"
+            height={theme.spacing(20)}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
             <Avatar
               sx={{ height: theme.spacing(18), width: theme.spacing(18) }}
               src="https://scontent.ffln1-1.fna.fbcdn.net/v/t1.6435-9/46501964_10215247847922755_2301475907941957632_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=84a396&_nc_eui2=AeE5JjNT9M_P_KMFPPh2I3xXvH6nKE2NOjW8fqcoTY06NdSnqm37GnWKkWKdjkDyWB8&_nc_ohc=7Ho2J5xlx2UAX_Zsa2C&_nc_ht=scontent.ffln1-1.fna&oh=00_AfDw9QOw6lktKW75FPZNGO9thIxBeae5jav9_CWUiWlI_Q&oe=6394C8AB"
@@ -33,11 +59,10 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
               </ListItemButton>
             </List>
           </Box>
-
         </Box>
       </Drawer>
 
-      <Box height="100vh" marginLeft={theme.spacing(28)}>
+      <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
         {children}
       </Box>
     </>
