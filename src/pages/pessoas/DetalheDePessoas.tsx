59d6a6bb50e388f75c9113b22e28cfa1117884a5
kayrongradilone <kayrongradilone@hotmail.com>
2022-11-25 17:41:04 -0300

    feat: criando hooks personalizados

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,23 +1,26 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { FerramentasDeDetalhes } from "../../shared/components";
-import { Form } from "@unform/web";
+import { Box, Grid, LinearProgress, Paper, Typography } from "@mui/material";
+
+
+
+import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 
 import { LayoutBaseDePagina } from "../../shared/layouts";
-import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
-import { VTextField } from "../../shared/forms";
-import { FormHandles } from "@unform/core";
+import { VTextField, VForm , useVForm } from "../../shared/forms";
+import { FerramentasDeDetalhes } from "../../shared/components";
 
 interface IFormData {
   email: string;
-  nomeCompleto: string;
   cidadeId: number;
+  nomeCompleto: string;
 }
 
 export const DetalheDePessoas: React.FC = () => {
   const { id = "nova" } = useParams<"id">();
   const navigate = useNavigate();
-  const formRef = useRef<FormHandles>(null);
+
+  const {formRef , isSaveAndClose,save,saveAndClose} = useVForm();
 
   const [isLoading, setIsLoading] = useState(false);
   const [nome, setNome] = useState("");
@@ -34,37 +37,49 @@ export const DetalheDePessoas: React.FC = () => {
           navigate("/pessoas");
         } else {
           setNome(result.nomeCompleto);
-
           formRef.current?.setData(result);
         }
       });
+    }else {
+      formRef.current?.setData({
+        nomeCompleto: '',
+        email: '',
+        cidadeId: ''
+      })
     }
   }, [id]);
 
   const handleSave = (dados: IFormData) => {
     setIsLoading(true);
+
     if (id === "nova") {
       PessoasService.create(dados).then((result) => {
         setIsLoading(false);
+
         if (result instanceof Error) {
           alert(result.message);
         } else {
-          navigate(`/pessoas/detalhe/${result}`);
+          if(isSaveAndClose()){
+            navigate(`/pessoas`);
+          }else {
+            navigate(`/pessoas/detalhe/${result}`);
+          }
         }
       });
     } else {
-      setIsLoading(true);
-      if (id === "nova") {
-        PessoasService.updateById(Number(id), {
-          id: Number(id),
-          ...dados,
-        }).then((result) => {
+      PessoasService.updateById(Number(id), { id: Number(id), ...dados }).then(
+        (result) => {
           setIsLoading(false);
+
           if (result instanceof Error) {
             alert(result.message);
+          }else {
+            if(isSaveAndClose()){
+              navigate(`/pessoas`);
+            }
           }
-        });
-      }
+        }
+      );
     }
   };
 
@@ -90,19 +105,73 @@ export const DetalheDePessoas: React.FC = () => {
           mostrarBotaoSalvarEFechar
           mostrarBotaoNovo={id !== "nova"}
           mostrarBotaoApagar={id !== "nova"}
-          aoClicarEmSalvar={() => formRef.current?.submitForm()}
-          aoClicarEmSalvarEFechar={() => formRef.current?.submitForm()}
+          aoClicarEmSalvar={save}
+          aoClicarEmSalvarEFechar={saveAndClose}
           aoClicarEmVoltar={() => navigate("/pessoas")}
           aoClicarEmApagar={() => handleDelete(Number(id))}
           aoClicarEmNovo={() => navigate("/pessoas/detalhe/nova")}
         />
       }
     >
-      <Form ref={formRef} onSubmit={handleSave}>
-        <VTextField placeholder="Nome Completo" name="nomeCompleto" />
-        <VTextField placeholder="Email" name="email" />
-        <VTextField placeholder="Cidade Id" name="cidadeId " />
-      </Form>
+      <VForm ref={formRef} onSubmit={handleSave}>
+        <Box
+          margin={1}
+          display="flex"
+          flexDirection="column"
+          component={Paper}
+          variant="outlined"
+        >
+          <Grid container direction="column" padding={2} spacing={2}>
+            {isLoading && (
+              <Grid item>
+                <LinearProgress variant="indeterminate" />
+              </Grid>
+            )}
+            {id === "nova" ? (
+              <Grid item>
+                <Typography variant="h6">Cadastro</Typography>
+              </Grid>
+            ) : (
+              <Grid item>
+                <Typography variant="h6">Editar Pessoa</Typography>
+              </Grid>
+            )}
+            <Grid container item direction="row" spacing={2}>
+              <Grid container item direction="row" spacing={2}>
+                <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
+                  <VTextField
+                    fullWidth
+                    label="Nome Completo"
+                    name="nomeCompleto"
+                    disabled={isLoading}
+                    onChange={(e) => setNome(e.target.value)}
+                  />
+                </Grid>
+              </Grid>
+              <Grid container item direction="row" spacing={2}>
+                <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
+                  <VTextField
+                    fullWidth
+                    label="Email"
+                    name="email"
+                    disabled={isLoading}
+                  />
+                </Grid>
+              </Grid>
+              <Grid container item direction="row" spacing={2}>
+                <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
+                  <VTextField
+                    fullWidth
+                    label="Cidade"
+                    name="cidadeId"
+                    disabled={isLoading}
+                  />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
+      </VForm>
     </LayoutBaseDePagina>
   );
 };
--- src/shared/forms/VTextField.tsx
+++ src/shared/forms/VTextField.tsx
@@ -25,9 +25,9 @@ return (
     error={!!error}
     helperText={error}
     defaultValue={defaultValue}
-    onKeyDown={() => error ? clearError() : undefined}
+    onKeyDown={(e) => {error && clearError(); rest.onKeyDown?.(e)}}
     value={value}
-    onChange={e => setValue(e.target.value)}
+    onChange={e => {setValue(e.target.value); rest.onChange?.(e)}}
     />
 )
 }
